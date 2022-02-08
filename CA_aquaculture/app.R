2067bafd5836a65444e974b3d8bea3ae0906e57e
Mae Rennick <maerennick@gmail.com>
2022-02-08 14:38:48 -0800

    outline of shiny app

--- CA_aquaculture/app.R
+++ CA_aquaculture/app.R
@@ -1,49 +1,68 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
 
 library(shiny)
+library(tidyverse)
+library(bslib)
+library(shinythemes)
+library(here)
+library(readxl)
+library(janitor)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+# read in data
+cal_data <- read_csv(here("aquaculture_data.csv")) %>%
+    drop_na() %>%
+    clean_names() %>%
+    pivot_longer(!year, names_to = "species", values_to = "landings")
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+# custom theme
+shiny_theme <- bs_theme(bootswatch = "minty")
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+# start shiny app
+ui <- fluidPage(theme = shiny_theme,
+                navbarPage("California Aquaculture",
+                           tabPanel("Home",
+                                    sidebarLayout(
+                                        sidebarPanel(),
+                                        mainPanel(
+                                            h1("App Description"),
+                                            p("California Aquauclture through time by year, species and leasing processes"
+                                            ),
+                                            br(),
+                                            p("The purpose of this Shiny App is to explore aquaculture landings data from the California Department of Fish and Wildlife (1971-2018) to better understand how aquaculture practices have changed through time."
+                                            ),
+                                        ), #end mainpanel
+                                    ) # end sidebarlayout
+                           ), #end tabPanel 1
+                           tabPanel("Farm Map",
+                                    sidebarLayout(
+                                        sidebarPanel(
+                                            checkboxGroupInput(inputId = "pick_species",
+                                                               label = "Species:",
+                                                               choices = unique(cal_data$species)
+                                            ) # end checkboxGroupInput
+                                        ), #end sidebarPanel
+                                        mainPanel("FARM MAP",
+                                                  plotOutput("cal_plot1"))
+                                    ) # end sidebarLayout
+                           ), # end tabpanel 2
+                           tabPanel("PIER Maps",
+                                    sidebarLayout(
+                                        sidebarPanel(
+                                            checkboxGroupInput(inputId = "pier_maps",
+                                                               label = "PIER maps:",
+                                                               choices = unique(cal_data$species)
+                                            ) # end checkboxGroupInput
+                                        ), #end sidebarPanel
+                                        mainPanel("PIER Map",
+                                                  plotOutput("cal_plot2"))
+                                    ) # end sidebarLayout
+                           ) # end tabpanel 3
+                ) #end navbar
+) # end ui
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+} # end output$cal_plot2
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
 
-# Run the application 
 shinyApp(ui = ui, server = server)
