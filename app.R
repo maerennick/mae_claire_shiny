207c81ee47a8539b868412519238f9a843cc2aab
Mae Rennick <maerennick@gmail.com>
2022-02-20 11:34:14 -0800

    new outputs

--- app.R
+++ app.R
@@ -8,11 +8,22 @@ library(here)
 library(readxl)
 library(janitor)
 
+
 # read in data
 cal_data <- read_csv(here("aquaculture_data.csv")) %>%
     drop_na() %>%
     clean_names() %>%
-    pivot_longer(!year, names_to = "species", values_to = "landings")
+    pivot_longer(!year, names_to = "species", values_to = "landings") %>%
+  mutate(group = case_when(species== "pacific_oyster" ~ "oyster",
+                           species== "kumamoto_oyster" ~ "oyster",
+                           species== "eastern_oyster" ~ "oyster",
+                           species==  "european_flat_oysters"~ "oyster",
+                           species==  "olympia_oysters"~ "oyster",
+                           species==  "clams"~ "clam",
+                           species==  "mussels"~ "mussel",
+                           species==  "abalone"~ "abalone"))
+
+text_data<- read_csv(here("species_info.csv"))
 
 # custom theme
 shiny_theme <- bs_theme(bootswatch = "yeti")
@@ -24,12 +35,19 @@ ui <- fluidPage(theme = shiny_theme,
                                     sidebarLayout(
                                         sidebarPanel(),
                                         mainPanel(
-                                            h1("Overview"),
-                                            p("California Aquauclture through time by year, species and leasing processes"
-                                            ),
+                                            h1("California Aquauclture through time"),
+                                            h3("An evaluation of development by production through time, species cultivated and the scape of current leasing processes"),
                                             br(),
+                                            h3("Overview"),
                                             p("The purpose of this Shiny App is to explore aquaculture landings data from the California Department of Fish and Wildlife (1971-2018) to better understand how aquaculture practices have changed through time."
                                             ),
+                                            br(),
+                                            h3("Background Information"),
+                                            p(" Aquaculture is one of the fastest growing food sectors in the world (Cotrell 2019, Love 2020, FAO 2020). Sustainable aquaculture -- balancing economic, ecological and social objectives to reduce negative impacts on a system (FAO 2020, Boyd et al. 2020, World Commission on Environment and Development 1987) -- is seen as a key path in addressing future food and economic development goals in the ‘Blue Economy’ (FAO 2020, FAO 2019, Tigchelaar et al. 2021, Short et al. 2021, Naylor et al. 2021, Österblom et al. 2020, Costello 2020). The state of California hosts 20 (SD ± 2) (mean # state farms freshwater and marine = 47) operational marine farms for the past 22 years, with marine production legally restricted to bivalve mollusks and select seaweeds (USDA National Agricultural Statistics Service 2018, Fong et al. 2022). Shellfish operations occur primarily in estuarine and intertidal state waters, although some production also occurs in land-based facilities. Further, most shellfish culture operations have some land- based facilities that can be used for hatching, early rearing, and processing of shellfish."),
+                                            br(),
+                                            imageOutput("aquaculture"),
+                                            br(),
+                                            h3("Citations:")
                                         ), #end main
                                     ) # end sidebar
                            ), #end tab
@@ -38,10 +56,9 @@ ui <- fluidPage(theme = shiny_theme,
                                         sidebarPanel(
                                             checkboxGroupInput(inputId = "pick_species",
                                                                label = "Species:",
-                                                               choices = list("Oysters" = 1, "Abalone" = 2, "Clams" = 3, "Mussels" = 4)
+                                                               choices = list("oysters" = 1, "abalone" = 2, "clams" = 3, "mussels" = 4)
                                             ),
-                                            sliderInput("slider2", label = h3("Slider Range"), min = 1971,
-                                                        max = 2018, value = c(1990, 2010))# end checkboxGroupInput
+                                            dateInput("date3", "Date:", value = "2012-02-29", format = "mm/dd/yy")# end checkboxGroupInput
                                         ), #end sidebarPanel
                                         mainPanel("FARM MAP",
                                                   plotOutput("cal_plot1"))
@@ -52,17 +69,52 @@ ui <- fluidPage(theme = shiny_theme,
                                         sidebarPanel(
                                             checkboxGroupInput(inputId = "species_info",
                                                                label = "PIER maps:",
-                                                               choices = list("Oysters" = 1, "Abalone" = 2, "Clams" = 3, "Mussels" = 4)
+                                                               choices = unique(cal_data$group)
                                             )# end checkboxGI
                                         ), #end sidebarPanel
-                                        mainPanel("Species Information",
-                                                  plotOutput("cal_plot2"))
+                                        mainPanel("Production by Species",
+                                                  plotOutput("cal_plot2"),
+                                                  textOutput("text"))
                                     ) # end sidebarLayout
                            ) # end tab
                 ) #end navbar
 )
 
 server <- function(input, output) {
+
+
+# output text for tab 3
+  cal_reactive_text <- reactive({
+    text_data %>%
+      filter(group %in% input$species_info)
+  })
+
+  # graph for tab 3
+
+  tableStart <- data.frame(Column1 = NA, Column2 = NA)
+
+  output$text <- renderTable({rbind(tableStart, cal_reactive_text())})
+
+
+# output plot for tab 3
+cal_reactive2 <- reactive({
+  cal_data %>%
+    filter(group %in% input$species_info)
+}) # end output$cal_plot 2
+
+# graph for tab 3
+output$cal_plot2 <- renderPlot(
+  ggplot(data = cal_reactive2(), aes(x = year, y = landings)) +
+    geom_point(aes(color = group))+
+    theme_minimal()+
+    labs(x= "Year",
+         y= "Landings (lbs)")
+
+
+) # end output$cal_plot1
+
+
+
 
 }
 
